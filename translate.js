f3a6dfda99626bcd7187c269ebd2df00079f2d5b
Samarpan Mohanty <86531335+Samarpan1122@users.noreply.github.com>
2023-07-02 09:46:13 +0530

    Add files via upload

--- translate.js
+++ translate.js
@@ -1,7 +1,16 @@
-$.cookie("googtrans", "/en/hi");
-
 function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'en', layout:
-    google.translate.TranslateElement.InlineLayout.SIMPLE},
-    'google_translate_element');
-    }
+    new google.translate.TranslateElement({
+        pageLanguage: 'en',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+    }, 'google_translate_element');
+}
+
+function hin() {
+    $.cookie("googtrans", "/en/hi");
+    location.reload();
+}
+
+function eng() {
+    $.cookie("googtrans", "/en/en");
+    location.reload();
+}
